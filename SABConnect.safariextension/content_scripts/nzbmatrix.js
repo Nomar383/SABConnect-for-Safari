c827473a43c878b6aa756c732cf282b2df5027bd
Kalen Hansen <nobel.nomar@gmail.com>
2012-07-06 18:03:04 -0700

    Slight edit to the fix for nzbxxx. Version bump.

--- SABConnect.safariextension/content_scripts/nzbmatrix.js
+++ SABConnect.safariextension/content_scripts/nzbmatrix.js
@@ -22,10 +22,8 @@ function findNZBIdMatrix(elem) {
    var hostname = window.location.href.substr(0, window.location.href.indexOf('/', 8));
    if (hostname.indexOf('nzbxxx') != -1) {
       url = url.replace('nzb-download.php', 'v1.1/download.php') + '&apikey=' + nzbxxx_apikey + '&username=' + nzbxxx_username;
-
-      if (hostname == "http://nzbxxx.com") {
-        hostname = "http://api.nzbxxx.com";
-      }
+       
+      hostname = "http://api.nzbxxx.com";
    } else {
       // 0.5+ needs nzb-details not nzb-download in url
       url = url.replace('nzb-download', 'nzb-details');
